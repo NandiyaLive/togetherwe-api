d58d14ece911bc553b7b57cb66d618dd4adac60f
Neranjana Prasad | Nandiya <neranjanaprasad@gmail.com>
2022-09-13 15:57:53 +0000

    Update donation.js

--- pages/api/donation.js
+++ pages/api/donation.js
@@ -2,5 +2,7 @@ export default async (_, res) => {
   const query = req.query;
   const {id} = query
   
-  return res.status(200).json({ id });
+  const data = [id]
+  
+  return res.status(200).json({ data });
 };
