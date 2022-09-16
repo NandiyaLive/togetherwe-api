75547343338fbeb3ac6e13c856c16904be251cf2
Neranjana Prasad <30690726+NandiyaLive@users.noreply.github.com>
2022-09-16 22:43:06 +0530

    Update amount.js

--- pages/api/amount.js
+++ pages/api/amount.js
@@ -1,7 +1,29 @@
-export default async (_, res) => {
-  const data = [10,20,50,100,500]
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-  res.setHeader("Cache-Control", "public, s-maxage=60, stale-while-revalidate=30");
+const handler = (req, res) => {
+  const d = new Date();
+
+  const data = [10,20,50,100,500]
 
   return res.status(200).json({ data });
 };
+
+module.exports = allowCors(handler);
