7d01cc3e394bef8374e54423556958fe3fa5fe77
Neranjana Prasad <30690726+NandiyaLive@users.noreply.github.com>
2022-09-16 22:42:23 +0530

    Update fundraising.js

--- pages/api/fundraising.js
+++ pages/api/fundraising.js
@@ -1,4 +1,26 @@
-export default async (_, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (req, res) => {
+  const d = new Date();
+
   const data = [
       { id: "AEaTUnvneik", title: "Pleiku city", urls: { raw: "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwxfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1", full: "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?crop=entropy&cs=srgb&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwxfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=85", regular: "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwxfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=1080", small: "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwxfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=400", thumb: "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwxfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=200" }, target_amount: 40000 },
       { id: "3eOIKv495OU", title: "Bloomington, Indiana", urls: { raw: "https://images.unsplash.com/photo-1607623618478-384d71c091d0?ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwyfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1", full: "https://images.unsplash.com/photo-1607623618478-384d71c091d0?crop=entropy&cs=srgb&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwyfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=85", regular: "https://images.unsplash.com/photo-1607623618478-384d71c091d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwyfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=1080", small: "https://images.unsplash.com/photo-1607623618478-384d71c091d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwyfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=400", thumb: "https://images.unsplash.com/photo-1607623618478-384d71c091d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHwyfHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=200" }, target_amount: 30000 },
@@ -8,7 +30,7 @@ export default async (_, res) => {
       { id: "uaPaEM7MiQQ", title: "Stuttgart, Germany", urls: { raw: "https://images.unsplash.com/photo-1542810634-71277d95dcbb?ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHw2fHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1", full: "https://images.unsplash.com/photo-1542810634-71277d95dcbb?crop=entropy&cs=srgb&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHw2fHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=85", regular: "https://images.unsplash.com/photo-1542810634-71277d95dcbb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHw2fHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=1080", small: "https://images.unsplash.com/photo-1542810634-71277d95dcbb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHw2fHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=400", thumb: "https://images.unsplash.com/photo-1542810634-71277d95dcbb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDkwNjV8MHwxfHNlYXJjaHw2fHxjaGlsZHJlbi1kb25hdGlvbnxlbnwwfDB8fA&ixlib=rb-1.2.1&q=80&w=200" }, target_amount: 50000 }
     ]
 
-  res.setHeader("Cache-Control", "public, s-maxage=60, stale-while-revalidate=30");
-
   return res.status(200).json({ data });
 };
+
+module.exports = allowCors(handler);
