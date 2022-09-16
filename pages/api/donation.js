0cba4aa004ed9d5899756fb76d8fd34422c537cb
Neranjana Prasad <30690726+NandiyaLive@users.noreply.github.com>
2022-09-16 22:44:14 +0530

    Update donation.js

--- pages/api/donation.js
+++ pages/api/donation.js
@@ -1,4 +1,26 @@
-export default async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (req, res) => {
+  const d = new Date();
+
   const query = req.query;
   const { id } = query
   
@@ -522,6 +544,8 @@ export default async (req, res) => {
   ]
 }
   const data = donations[id]
-    
+
   return res.status(200).json({ data });
 };
+
+module.exports = allowCors(handler);
